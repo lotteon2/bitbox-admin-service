ae631989adcf535f7692db1e9888e88dc262efb7
hyevvy <hyeg0@naver.com>
2023-10-19 16:40:51 +0900

    [FEAT] password 암호화 추가

--- src/main/java/com/bitbox/admin/dto/AdminDto.java
+++ src/main/java/com/bitbox/admin/dto/AdminDto.java
@@ -4,6 +4,7 @@ import com.bitbox.admin.domain.Admin;
 import io.github.bitbox.bitbox.enums.AuthorityType;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -22,6 +23,7 @@ public class AdminDto {
         return Admin.builder()
                 .adminName(adminDto.getAdminName())
                 .adminEmail(adminDto.getAdminEmail())
+                .adminPassword(new BCryptPasswordEncoder().encode("1111"))
                 .adminAuthority(adminDto.getAdminAuthority())
                 .build();
     }
