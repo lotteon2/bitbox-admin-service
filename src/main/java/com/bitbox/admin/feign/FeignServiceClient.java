a7bd73b81485d7c00df6030ab357124d83b0f5f3
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-10-23 10:04:40 +0900

    [REFACTORING] - 사용 안되는 코드 제거 - feign 관련 코드 수정

--- src/main/java/com/bitbox/admin/feign/FeignServiceClient.java
+++ src/main/java/com/bitbox/admin/feign/FeignServiceClient.java
@@ -4,14 +4,12 @@ import io.github.bitbox.bitbox.dto.MemberTraineeResult;
 import io.github.bitbox.bitbox.dto.MemberValidDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "feignClient",
-        url = "localhost:8000")
+@FeignClient(name = "user-service")
 public interface FeignServiceClient {
     @PostMapping(value="/user-service/member/admin/check")
     ResponseEntity<MemberTraineeResult> getMemberIsValidToAddExam(@RequestBody List<MemberValidDto> memberValidDto);
