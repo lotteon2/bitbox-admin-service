5a7ccc4adf2a1eda9392029e3f14034552cccdf0
hyevvy <hyeg0@naver.com>
2023-10-31 17:57:10 +0900

    [FEAT] 클래스 추가

--- src/main/java/com/bitbox/admin/service/ClassService.java
+++ src/main/java/com/bitbox/admin/service/ClassService.java
@@ -1,11 +1,16 @@
 package com.bitbox.admin.service;
 
+import com.bitbox.admin.domain.Admin;
+import com.bitbox.admin.domain.ClassAdmin;
 import com.bitbox.admin.domain.Classes;
+import com.bitbox.admin.domain.key.ClassAdminId;
 import com.bitbox.admin.dto.ClassDto;
 import com.bitbox.admin.dto.ClassInfoWithAdminList;
 import com.bitbox.admin.dto.ClassUpdateDto;
 import com.bitbox.admin.exception.DuplicationException;
 import com.bitbox.admin.exception.InvalidClassIdException;
+import com.bitbox.admin.repository.AdminInfoRepository;
+import com.bitbox.admin.repository.ClassAdminInfoRepository;
 import com.bitbox.admin.repository.ClassInfoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +18,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -20,9 +26,11 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class ClassService {
     private final ClassInfoRepository classInfoRepository;
+    private final ClassAdminInfoRepository classAdminInfoRepository;
+    private final AdminInfoRepository adminInfoRepository;
 
     @Transactional
-    public Long registerClassInfo(ClassDto classDto) {
+    public Long registerClassInfo(String adminId, ClassDto classDto) {
         if(classInfoRepository.existsByClassCode(classDto.getClassCode())){
             throw new DuplicationException("반 생성 | 중복 코드 에러");
         }
@@ -30,7 +38,13 @@ public class ClassService {
             throw new DuplicationException("반 생성 | 중복 이름 에러");
         }
 
-        return classInfoRepository.save(classDto.convertExamDtoToExam(classDto)).getClassId();
+        Admin admin = adminInfoRepository.findById(adminId).orElseThrow(()->new RuntimeException("exception"));
+        Classes classes = classInfoRepository.save(classDto.convertExamDtoToExam(classDto));
+        ClassAdminId classAdminId = new ClassAdminId(classes.getClassId(), adminId);
+        ClassAdmin classAdmin = new ClassAdmin(classAdminId, classes, admin);
+        classAdminInfoRepository.save(classAdmin);
+
+        return classes.getClassId();
     }
 
     @Transactional
