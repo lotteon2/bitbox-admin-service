e86b422024868afc57937ae2a200f76125a54c66
hyevvy <hyeg0@naver.com>
2023-11-01 14:35:50 +0900

    [FEAT] 성적 조회시 교육생만 조회

--- src/main/java/com/bitbox/admin/repository/MemberInfoRepository.java
+++ src/main/java/com/bitbox/admin/repository/MemberInfoRepository.java
@@ -1,11 +1,13 @@
 package com.bitbox.admin.repository;
 
 import com.bitbox.admin.domain.MemberInfo;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 
 public interface MemberInfoRepository extends CrudRepository<MemberInfo, String> {
+    @Query(value = "SELECT mi FROM MemberInfo mi WHERE mi.classes.classId = :classId AND mi.memberAuthority = 'TRAINEE'")
     List<MemberInfo> findByClasses_ClassId(Long classId);
 }
