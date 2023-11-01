0ab4a31c76ca83909c76c779ea49caa56eadba3a
hyevvy <hyeg0@naver.com>
2023-11-01 14:38:52 +0900

    [FEAT] 학생이 본인 반 성적 조회시 교육생만 조회

--- src/main/java/com/bitbox/admin/repository/ExamInfoRepository.java
+++ src/main/java/com/bitbox/admin/repository/ExamInfoRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 public interface ExamInfoRepository extends CrudRepository<Exam, Long> {
     boolean existsByClasses_ClassIdAndExamNameAndDeletedIsFalse(Long classId, String examName);
 
-    @Query(value = "SELECT new com.bitbox.admin.service.response.ExamInfoResponse(e.examId, e.examName, c.classId, c.className, e.perfectScore, avg(g.score)) FROM Exam e INNER JOIN Grade g ON e.examId = g.exam.examId INNER JOIN Classes c ON e.classes.classId = c.classId WHERE e.classes.classId = :classId AND e.deleted = false GROUP BY e.examId")
+    @Query(value = "SELECT new com.bitbox.admin.service.response.ExamInfoResponse(e.examId, e.examName, c.classId, c.className, e.perfectScore, avg(g.score)) FROM Exam e INNER JOIN Grade g ON e.examId = g.exam.examId INNER JOIN Classes c ON e.classes.classId = c.classId WHERE e.classes.classId = :classId AND e.deleted = false AND g.memberInfo.memberAuthority = 'TRAINEE' GROUP BY e.examId")
     List<ExamInfoResponse> findExamByClasses_ClassIdAndDeletedIsFalse(Long classId);
 }
